a5c14bbf58e4613d0a606a2b94dd5806a93d031a
juhyung <juhyung@gmail.com>
2023-05-30 01:04:43 +0900

    lgoin update

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,24 +2,32 @@ import React from 'react';
 import styles from '../styles/login.module.css'
 
 const Login = () => {
+
     return (
-        <div>
+        <div className={styles.wrapper}>
             <form className={styles.container}>
-                <div className={styles.flex}>
-                    <input type='text' name='id' placeholder='아이디를 입력해주세요.'></input>
+                <h1>로그인</h1>
+                <div className={styles.inputBox}>
+                    <input className={styles.inputId} type='text' name='id' placeholder='아이디를 입력해주세요.'></input>
+                    <p className={styles.mailStyle}>@</p>
                     <select>
                         <option value="naver.com">naver.com</option>
+                        <option value="gmail.com">gmail.com</option>
                     </select>
                 </div>
-                <input type='password' name='password' placeholder='비밀번호를 입력하세요.'></input>
-                <select type="chekbox">
-                    <p>아이디/비밀번호 기억하기</p>
-                </select>
+                <div className={styles.inputBox}>
+                    <input className={styles.inputPassword} type='password' name='password' placeholder='비밀번호를 입력하세요.'></input>
+                </div>
+                
+                <div className={styles.checkBox}>
+                    <input className={styles.inputCheckBox} type="checkbox"/>
+                    <p className={styles.fontStyle}>아이디/비밀번호 기억하기</p>
+                </div>
 
-                <button type='button'>로그인</button>
-                <button type='button'>회원가입</button>
+                <button className={styles.loginBtn} type='button'>로그인</button>
+                <button className={styles.signUpBtn} type='button'>회원가입</button>
 
-                <p>아이디/비밀번호 기억이 나지 않아요.</p>
+                <p className={styles.forgotFontStyle}>아이디/비밀번호 기억이 나지 않아요.</p>
             </form>
         </div>
     );
