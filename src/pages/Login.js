78d4135ccd70da55bd4d6a87d17424d5d63a7df4
juhyung <juhyung@gmail.com>
2023-05-30 21:51:53 +0900

    login update

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,22 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from '../styles/login.module.css'
 
 const Login = () => {
+
+    const [isIdActive, setIsIdActive] = useState(false);
+    const [isPasswordActive, setIsPasswordIdActive] = useState(false);
+    const handleInputIdFocus = () => {
+        setIsIdActive(true);
+    }
+
+    const hadnleInputIdBlur = () => {
+        setIsIdActive(false);
+    }
+
+    const handleInputPasswordFocus = () => {
+        setIsPasswordIdActive(true);
+    }
+
+    const hadnleInputPasswordBlur = () => {
+        setIsPasswordIdActive(false);
+    }
 
     return (
         <div className={styles.wrapper}>
             <form className={styles.container}>
                 <h1>로그인</h1>
-                <div className={styles.inputBox}>
-                    <input className={styles.inputId} type='text' name='id' placeholder='아이디를 입력해주세요.'></input>
+                <div className={isIdActive ? styles.inputBoxActive : styles.inputBox}>
+                    <input 
+                        className={styles.inputId}
+                        onBlur={hadnleInputIdBlur} 
+                        onFocus={handleInputIdFocus} 
+                        type='text' 
+                        name='id' 
+                        placeholder='아이디를 입력해주세요.'>
+                    </input>
                     <p className={styles.mailStyle}>@</p>
-                    <select>
+                    <select 
+                        onFocus={handleInputIdFocus} 
+                        onBlur={hadnleInputIdBlur}
+                    >
                         <option value="naver.com">naver.com</option>
                         <option value="gmail.com">gmail.com</option>
                     </select>
                 </div>
-                <div className={styles.inputBox}>
-                    <input className={styles.inputPassword} type='password' name='password' placeholder='비밀번호를 입력하세요.'></input>
+                <div className={isPasswordActive ? styles.inputBoxActive : styles.inputBox}>
+                    <input 
+                        className={styles.inputPassword} 
+                        onBlur={hadnleInputPasswordBlur} 
+                        onFocus={handleInputPasswordFocus} 
+                        type='password' 
+                        name='password' 
+                        placeholder='비밀번호를 입력하세요.'>
+                    </input>
                 </div>
                 
                 <div className={styles.checkBox}>
